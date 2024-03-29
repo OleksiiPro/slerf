666ab3881bbb255bf80ab1a1936b2a0ac74fa2fa
Oleksii Proniakin <oleksii@Oleksiis-MacBook-Pro.local>
2024-03-29 01:21:17 -0700

    add

--- netlify/functions/streamr.ts
+++ netlify/functions/streamr.ts
@@ -4,22 +4,28 @@ import { StreamrClient } from '@streamr/sdk';
 
 export async function handler(event: HandlerEvent, context: HandlerContext) {
 
-  const streamr = new StreamrClient();
-
-  const messageData = {
-    hello: 'world',
+  const wallet = {
+    address: '0x4FC957F47ab6f84c34544268b6510317675126d0',
+    privateKey: '0x6456d224bf80acbf37bf2524d9db59eed5bed239b8b4d981dcba3d43162ca4a7'
   };
 
-  try {
-    const message = await streamr.publish('0x36e855b6358e977832bde8b762636fdfb1c962d1/slerf', messageData);
-  } catch (error) {
-    console.log(error);
-  }
+  const streamr = new StreamrClient({
+      auth: {
+          privateKey: wallet.privateKey,
+      },
+  });
+
+  const messageData = {
+    date: Date.now(),
+  };
+
+  
+  const message = await streamr.publish('0x36e855b6358e977832bde8b762636fdfb1c962d1/slerf', messageData);
 
   return {
     statusCode: 200,
     headers: { "content-type": "application/json" },
-    body: JSON.stringify(messageData)
+    body: JSON.stringify(message)
   };
 
 }
